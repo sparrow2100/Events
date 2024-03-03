146af48e6b030f5edf302104b6364afdfde3ed9e
sparrow2100 <111162787+sparrow2100@users.noreply.github.com>
2024-03-03 12:19:55 -0500

    Add integration tests

--- src/__tests__/EventList.test.js
+++ src/__tests__/EventList.test.js
@@ -1,9 +1,10 @@
 /* eslint-disable testing-library/prefer-presence-queries */
 /* eslint-disable testing-library/render-result-naming-convention */
 /* eslint-disable testing-library/prefer-screen-queries */
-import { render } from "@testing-library/react";
+import { render, within, waitFor } from "@testing-library/react";
 import EventList from "../components/EventList";
 import { getEvents } from "../api";
+import App from "../App";
 
 describe("<EventList /> component", () => {
   test('has an element with "list" role', () => {
@@ -16,5 +17,17 @@ describe("<EventList /> component", () => {
     expect(EventListComponent.getAllByRole("listitem")).toHaveLength(
       allEvents.length
     );
+  });
+});
+
+describe("<EventList /> integration", () => {
+  test("renders a list of 32 events when the app is mounted and rendered", async () => {
+    const AppComponent = render(<App />);
+    const AppDOM = AppComponent.container.firstChild;
+    const EventListDOM = AppDOM.querySelector("#event-list");
+    await waitFor(() => {
+      const EventListItems = within(EventListDOM).queryAllByRole("listitem");
+      expect(EventListItems.length).toBe(32);
+    });
   });
 });
